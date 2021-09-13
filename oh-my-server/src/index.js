7e466cf14f60ecc48c66bced41ae63d5fba65079
Justiceserv <justiceserv@jserv.xyz>
2021-09-13 15:16:47 +0900

    [Add] Basic Structure

--- oh-my-server/src/index.js
+++ oh-my-server/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './main/App'
+import { BrowserRouter as Router , Route } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App()
+{
+  <Router>
+    <Route exact path="/" component={Main}></Route>
+  </Router>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
--- oh-my-server/src/main/App.js
+++ oh-my-server/src/main/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +11,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn NO
         </a>
       </header>
     </div>
